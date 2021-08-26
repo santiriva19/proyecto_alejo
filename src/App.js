689fbd56bf2477f16535f073766976ce96972e15
Santiago Rivadeneira Delgado <santiriva19@outlook.es>
2021-08-25 21:55:33 -0500

    gei el que lea

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Flight from './components/flight';
+import Img1 from './images/img1.jpg'
+import Img2 from './images/img2.jpg'
+import Img3 from './images/img3.png'
+import Img4 from './images/img4.jpg'
+import Img5 from './images/img5.jpg'
 function App() {
+  const dataFlights = [
+      {
+          "id": 0,
+          "title": "Miami",
+          "fromTo": "Bogota DC - Miami",
+          "price": 3000,
+          "img": Img1
+      },
+      {
+          "id": 1,
+          "title": "Cancun",
+          "fromTo": "Bogota DC - Cancun",
+          "price": 200,
+          "img": Img2
+      },
+      {
+          "id": 2,
+          "title": "Cali",
+          "fromTo": "Bogota DC - Cali",
+          "price": 400,
+          "img": Img3
+      },
+      {
+          "id": 3,
+          "title": "Cartagena",
+          "fromTo": "Bogota DC - Cartagena",
+          "price": 160,
+          "img": Img4
+      },
+      {
+          "id": 4,
+          "title": "Paris",
+          "fromTo": "Bogota DC - Paris",
+          "price": 3900,
+          "img": Img5
+      },
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bodyHome">
+      <div className="head">
+        <h1> The best way to buy your flights </h1>
+      </div>
+      <div className="contFlights">
+        <div className="rowFlight">
+          {
+            dataFlights.map((flight) => 
+              <Flight 
+              index={flight.index}
+              title={flight.title} 
+              price={flight.price}  
+              fromTo={flight.fromTo}
+              img={flight.img}
+              />
+            )
+          }
+        </div>
+       
+      </div>
     </div>
   );
 }
---
+++ src/components/flight.js
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+import './flight.css'
+export default function Flight(props) {
+    const [priceEdited, setPriceEdited] = useState(props.price);
+    const [timesAdded, setTimesAdded] = useState(1);
+    const plus = () => {
+        let auxCounter = timesAdded;
+        if(timesAdded !== 0 )
+        {
+            auxCounter++;
+            setTimesAdded(auxCounter);
+            setPriceEdited(props.price * auxCounter);
+        }
+        
+    }
+    const minus = () => {
+        let auxCounter = timesAdded;
+        if(timesAdded > 1 )
+        {
+            auxCounter--;
+            setTimesAdded(auxCounter);
+            setPriceEdited(props.price * auxCounter);
+        }
+    }
+    return(
+        <div className="flightCard">
+            <div className="imageCard">
+                <img
+                src={props.img} 
+                />
+            </div>
+            <div className="infoCard">
+                <h2> {props.title} </h2>
+                <p> {priceEdited}$ </p>
+                <div className="buttonGroup">
+                    <input 
+                    onClick={ () => minus()}
+                    type="button" 
+                    value="-" 
+                    />
+                    <input 
+                    onClick={ () => plus()}
+                    type="button" 
+                    value="+" 
+                    />
+                    {timesAdded}
+                </div>
+            </div>
+           
+        </div>
+    )
+}
